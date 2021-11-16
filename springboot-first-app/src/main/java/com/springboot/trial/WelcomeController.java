e96ac001f4b5310e65327cdccc1039ccb881f1cd
Jijikoshy88 <81867861+Jijikoshy88@users.noreply.github.com>
2021-11-16 11:52:28 +0530

    Add files via upload

---
+++ springboot-first-app/src/main/java/com/springboot/trial/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.springboot.trial;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class WelcomeController {
+
+	
+		@GetMapping("/welcome")
+		public String welcome()
+		{
+			return "welcome to spring boot app development";
+		}
+		
+
+	
+
+}
